63ea0887e4c11d36ffa819624ee173a10c7a936d
Piotr Kontowicz <przemyslowiec@gmail.com>
2019-01-23 16:31:55 +0100

    Solve all issues with day 41

--- day_41.py
+++ day_41.py
@@ -8,20 +8,47 @@
 # you should return the list ['A', 'B', 'C', 'A', 'C'] even though ['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first one is lexicographically smaller.
 
 def getPath(array, start):
+    array.sort(key=lambda x:x[-1])
+
+
     dictionay = {}
+    dictionay_second = {}
     for element in array:
-        dictionay[element[0]] = element[-1]
+        if element[0] not in dictionay:
+            dictionay[element[0]] = element[-1]
+        else:
+            dictionay_second[element[0]] = element[-1]
+
     path = []
     if start not in dictionay:
         return None
-
+    before = start
     path.append(start)
-    while start in dictionay:
-        start = dictionay[start]
-        path.append(start)
 
+    while start in dictionay:
+        before = start
+        start = dictionay[start]
+        if before in  dictionay_second:
+            dictionay[before] = dictionay_second[before]
+            del dictionay_second[before]
+            path.append(start)
+            continue
+
+        del dictionay[before]
+
+        path.append(start)
+    if len(dictionay) > 0:
+        return None
     return path
 
 print([('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')] )
 print(getPath([['SFO', 'HKO'], ['YYZ', 'SFO'], ['YUL', 'YYZ'], ['HKO', 'ORD']], 'YUL' ))
 assert getPath([['SFO', 'HKO'], ['YYZ', 'SFO'], ['YUL', 'YYZ'], ['HKO', 'ORD']], 'YUL' ) == ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD']
+
+print([['SFO', 'COM'], ['COM', 'YYZ']])
+print(getPath([['SFO', 'COM'], ['COM', 'YYZ']], 'COM'))
+assert getPath([['SFO', 'COM'], ['COM', 'YYZ']], 'COM') == None
+
+print([['SFO', 'COM'], ['COM', 'YYZ']])
+print(getPath([['A', 'B'], ['A', 'C'], ['B', 'C'], ['C', 'A']], 'A'))
+assert getPath([['A', 'B'], ['A', 'C'], ['B', 'C'], ['C', 'A']], 'A') == ['A', 'B', 'C', 'A', 'C']
