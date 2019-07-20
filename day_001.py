de35dffe0a60cb2e36d8e1d8b2ec90d0f550ee6b
Piotr Kontowicz <przemyslowiec@gmail.com>
2019-07-20 20:57:02 +0200

    Refactor.

--- day_001.py
+++ day_001.py
@@ -1,22 +1,21 @@
 # Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-table = [10, 15, 3, 7]
-k = 17
-
-def sumUpTo(table, k):
+def aum_up_to(table, k):
     table.sort()
+	
     begin = 0
     end = len(table) - 1
+	
     while begin < end:
-        s = table[begin] + table[end]
-        if s == k:
-            print('Yes')
-            return
-        elif s > k:
+        tmp_sum = table[begin] + table[end]
+        if tmp_sum == k:
+            return True
+        elif tmp_sum > k:
             end = end - 1
-        elif s < k:
+        elif tmp_sum < k:
             begin = begin + 1
-    print('No')
+    return False
 
-sumUpTo(table, 100)
+table = [10, 15, 3, 7]
+aum_up_to(table, 17)
